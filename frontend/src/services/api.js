d97c53ccc7f8a49bdc6e61ca9a43a68d509973f1
devallservicess <traintobe49@gmail.com>
2026-02-12 23:02:34 +0100

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8081/api';
+const API_URL = 'https://ecowaste-manager.onrender.com/api';
 
 const api = axios.create({
     baseURL: API_URL,
